43650320e02ca772816867f1175942832b271660
thiago <thiagosilva128@gmail.com>
2021-10-02 14:00:58 -0300

    create global style with Styled-components

---
+++ src/global.js
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+    }
+
+    a {
+        color: inherit;
+        text-decoration: none;
+    }
+
+    
+    html {
+        @media(max-width: 1080px) {
+            font-size: 93.75%;
+        }
+        @media(max-width: 720px) {
+            font-size: 87.5%;
+        }
+    }
+`
